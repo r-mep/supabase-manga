d48be0cfc3e528ee77dbe5cb287a39566fafcf90
risa mehara <r.mehara.224@gmail.com>
2021-08-18 23:21:13 +0900

    create function to add title to database

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,59 @@
 import { Auth, Button, IconLogOut } from "@supabase/ui";
-import type { ReactNode } from "react";
-import React from "react";
+import React, { ReactNode, useCallback, useEffect, useState } from "react";
 import { LayoutWrapper } from "../components/layoutWrapper";
+import { Title, TitleList } from "../components/titleList";
 import { client } from "../libs/supabase";
 
 type Props = {
   children: ReactNode;
 };
 
+// DBから漫画タイトルを取得
+const getTitles = async () => {
+  const { data, error } = await client
+    .from("manga_title")
+    .select("*")
+    .order("title");
+  if (!error && data) {
+    return data;
+  }
+  return [];
+};
+
 const Container = (props: Props) => {
   const { user } = Auth.useUser();
+  const [text, setText] = useState<string>("");
+  const [titles, setTitles] = useState<Title[]>([]);
+
+  // DBから取得した漫画タイトルをセット
+  const getTitleList = useCallback(async () => {
+    const data = await getTitles();
+    console.log({ data })
+    setTitles(data);
+  }, []);
+
+  useEffect(() => {
+    getTitleList();
+  }, [user, getTitleList]);
 
   // ログインしている場合
   if (user) {
     return (
       <div>
+        <div className="flex justify-center gap-2 p-4">
+          <input
+            className="w-full h-12 px-4 bg-white border border-gray-300 rounded shadow appearance-none hover:border-gray-700"
+            placeholder="Filtering text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <TitleList
+          titles={titles}
+          uuid={user.id}
+          getTitleList={getTitleList}
+          filterText={text}
+        />
         <div className="flex justify-end mx-2 my-4">
           <Button
             size="medium"
