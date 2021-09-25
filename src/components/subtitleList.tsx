13f9be5eed09282aeb1b54a9e77bc06f78dd0c70
risa mehara <r.mehara.224@gmail.com>
2021-09-26 02:50:02 +0900

    create page title

---
+++ src/components/subtitleList.tsx
@@ -0,0 +1,35 @@
+import { AddSubtitle } from "./addSubtitle";
+import { SubtitleCard } from "./subtitleCard";
+import { Title } from "./titleList";
+import { subtitle } from "../pages/title";
+
+type Props = {
+  subtitles: subtitle[];
+  title: Title;
+  uuid: string;
+  getSubtitleList: VoidFunction;
+};
+
+export const SubtitleList = (props: Props) => {
+  return (
+    <div className="grid grid-cols-3 gap-2 m-4 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 2xl:grid-cols-8">
+      <AddSubtitle
+        title={props.title}
+        uuid={props.uuid}
+        getSubtitleList={props.getSubtitleList}
+      />
+      {props.subtitles.map((subtitle) => {
+        return (
+          <div key={subtitle.id}>
+            <SubtitleCard
+              subtitle={subtitle}
+              title={props.title}
+              uuid={props.uuid}
+              getSubtitleList={props.getSubtitleList}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
